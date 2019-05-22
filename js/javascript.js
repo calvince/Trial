adf17931dc09c50b798a96be7340d5d932200bf3
Omonge Calvince <calvo-linus@localhost.localdomain>
2019-05-22 18:56:36 +0300

    added javascript

---
+++ js/javascript.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+  // var hussein = new Typed ('.h1-1-1',{
+  //   strings:[
+  //     'AMB.MISHI MASIKA MWATSAHU'
+  //   ],
+  //   typeSpeed:60,
+  //   backSpeed:60,
+  //   loop:true
+  // });
+  // $('button#green').click(function(){
+  //   $('body').removeClass();
+  //   $('body').addClass('green-background');
+  // });
+
+  // $('button#yellow').click(function(){
+  //   $('body').removeClass();
+  //   $('body').addClass('yellow-background');
+  // });
+
+  // $('button#red').click(function(){
+  //   $('body').removeClass();
+  //   $('body').addClass('red-background');
+  // });
+  // $('button#hello').click(function(){
+  //   $('ul#user').prepend('<li>Hello!</li>');
+  //   $('ul#user').children('li').first().click(function(){
+  //     alert('hi');
+  //   })
+  //   $('ul#webpage').prepend('<li>Why hello there!</li>');
+  //   // $('li').click(function(){
+  //   //   alert('hi');
+  //   // })
+  // });
+  // $('button#goodbye').click(function(){
+  //   $('ul#user').prepend('<li>Goodbye!</li>');
+  //   $('ul#user').children('li').first().click(function(){
+  //     alert('hi');
+  //   })
+  // });
+  // $('button#stop').click(function(){
+  //   $('ul').prepend('<li>Stop copying me!</li>');
+  // });
+  // $('#blanks form').submit(function(event){
+    //  var person1Input = $('input#person1').val();
+    //  var person2Input = $("input#person2").val();
+    //  var animalInput = $("input#animal").val();
+    //  var exclamationInput = $("input#exclamation").val();
+    //  var verbInput = $("input#verb").val();
+    //  var nounInput = $("input#noun").val();
+
+    //  $('.person1').text(person1Input);
+    //  $(".person2").text(person2Input);
+    //  $(".animal").text(animalInput);
+    //  $(".exclamation").text(exclamationInput);
+    //  $(".verb").text(verbInput);
+    //  $(".noun").text(nounInput);
+    // $('#blanks form').submit(function(){
+    //   // dry code
+    //   var blanks = ['personi1','person2','animal','exclamation','verb','noun'];
+    //   blanks.forEach(function(blank){
+    //   //  getting the user inputs
+    //     var userInput = $('input#' + blank).val();
+    //     $('.' + blank).text(userInput);
+
+    //   });
+    });
+
+//     $('#story').show();
+//     event.preventDefault();
+//   })
+
+// });
+var circle = {
+
+  radius:1,
+  location:{
+    x:1,
+    y:1
+  },
+  draw: function(){
+    console.log('draw');
+  }
+};
+circle.draw();
